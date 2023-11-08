39fd1a25abda9b458f437f61aa01830270407f18
AnastasiiaBolbot <boo.anastasiia.boo@gmail.com>
2023-11-08 12:48:53 +0200

    ProjectLayout
    My third small project for my studies.
    
    Signed-off-by: AnastasiiaBolbot <boo.anastasiia.boo@gmail.com>

---
+++ ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Анастасия Болбот on 03.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
